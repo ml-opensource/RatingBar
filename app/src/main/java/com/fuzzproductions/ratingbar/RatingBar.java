f5f52ce30e336a704c8a63ffc86e6b912dd45cf9
Andre Perkins <aperkins@fuzzproductions.com>
2016-07-12 22:52:59 -0400

    [Customizations] allow user to provide different drawables than the default drawables used

--- app/src/main/java/com/fuzzproductions/ratingbar/RatingBar.java
+++ app/src/main/java/com/fuzzproductions/ratingbar/RatingBar.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Rect;
 import android.os.Build;
+import android.support.annotation.DrawableRes;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.Gravity;
@@ -18,10 +19,16 @@ import android.widget.LinearLayout;
  */
 public class RatingBar extends LinearLayout implements View.OnTouchListener {
     private static final String TAG = "SurveyRatingBar";
+    protected static final int DEFAULT_FILLED_DRAWABLE = R.drawable.icn_rating_start_green;
+    protected static final int DEFAULT_EMPTY_DRAWABLE = R.drawable.icn_rating_start_grey;
     private int mMaxCount = 5;
     private int currentlySelected;
     private int minSelected = 0;
     private int starSize = 0;
+    @DrawableRes
+    private int filledDrawable;
+    @DrawableRes
+    private int emptyDrawable;
 
     /**
      * Amount of space between consecutive rating stars - typically 5 dp.
@@ -31,18 +38,21 @@ public class RatingBar extends LinearLayout implements View.OnTouchListener {
     public RatingBar(Context context) {
         super(context);
         setOnTouchListener(null);
+        setDefaultDrawables();
         updateChildViews();
     }
 
     public RatingBar(Context context, AttributeSet attrs) {
         super(context, attrs);
         setOnTouchListener(null);
+        setDefaultDrawables();
         updateChildViews();
     }
 
     public RatingBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setOnTouchListener(null);
+        setDefaultDrawables();
         updateChildViews();
     }
 
@@ -51,9 +61,14 @@ public class RatingBar extends LinearLayout implements View.OnTouchListener {
     public RatingBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         setOnTouchListener(null);
+        setDefaultDrawables();
         updateChildViews();
     }
 
+    private void setDefaultDrawables() {
+        filledDrawable = DEFAULT_FILLED_DRAWABLE;
+        emptyDrawable = DEFAULT_EMPTY_DRAWABLE;
+    }
 
     public void setCurrentlySelectedPosition(int pos) {
         currentlySelected = pos;
@@ -113,14 +128,30 @@ public class RatingBar extends LinearLayout implements View.OnTouchListener {
             final ImageView v = (ImageView) getChildAt(i);
             //TODO: make below more dynamic... i guess
             if (i < currentlySelected) {
-                v.setImageResource(R.drawable.icn_rating_start_green);
+                v.setImageResource(filledDrawable);
             } else {
-                v.setImageResource(R.drawable.icn_rating_start_grey);
+                v.setImageResource(emptyDrawable);
             }
 
             v.postInvalidate();
         }
 
+    }
+
+    /**
+     * Changes the current filled drawable to the one passed in via the
+     * {@code filledDrawable}. */
+    public void setFilledDrawable(@DrawableRes int filledDrawable) {
+        this.filledDrawable = filledDrawable;
+        updateChildViews();
+    }
+
+    /**
+     * Changes the current empty drawable to the one passed in via the
+     * {@code emptyDrawable}. */
+    public void setEmptyDrawable(@DrawableRes int emptyDrawable) {
+        this.emptyDrawable = emptyDrawable;
+        updateChildViews();
     }
 
     protected void generateAndAddChildViewAndParams() {
