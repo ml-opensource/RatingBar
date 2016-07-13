9e1de64ddb0e3b51bcf552edd356a97fb13fe6a7
Piotr Leja <pleja@fuzzproductions.com>
2016-07-13 09:33:35 -0400

    replaced TAG with proper name

--- app/src/main/java/com/fuzzproductions/ratingbar/RatingBar.java
+++ app/src/main/java/com/fuzzproductions/ratingbar/RatingBar.java
@@ -18,7 +18,8 @@ import android.widget.LinearLayout;
  * @author Piotr Leja (FUZZ)
  */
 public class RatingBar extends LinearLayout implements View.OnTouchListener {
-    private static final String TAG = "SurveyRatingBar";
+    @SuppressWarnings("unused")
+    private static final String TAG = "RatingBar";
     protected static final int DEFAULT_FILLED_DRAWABLE = R.drawable.icn_rating_start_green;
     protected static final int DEFAULT_EMPTY_DRAWABLE = R.drawable.icn_rating_start_grey;
     private int mMaxCount = 5;
